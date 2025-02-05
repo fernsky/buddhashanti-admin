ea767254d8f32fb9591ee1c4e058b9102ee73f3c
Trilochan Bhusal <intensivestudy.research@gmail.com>
2025-02-05 13:05:02 +0545

    feat: add map layers

--- src/store/use-layer-store.ts
+++ src/store/use-layer-store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { produce } from "immer";
 import { Area } from "@/server/api/routers/areas/area.schema";
 import { Ward } from "@/server/api/routers/ward/ward.schema";
@@ -24,56 +25,69 @@ interface LayerState {
     visible: boolean,
   ) => void;
   initializeWardLayer: (wardNumber: string, areaIds: string[]) => void;
+
+  // Add UI state
+  isControlOpen: boolean;
+  setControlOpen: (open: boolean) => void;
 }
 
 export const useLayerStore = create<LayerState>()(
-  devtools((set) => ({
-    wards: [],
-    areas: [],
-    wardLayers: {},
+  devtools(
+    persist(
+      (set) => ({
+        wards: [],
+        areas: [],
+        wardLayers: {},
 
-    setWards: (wards) => set({ wards }),
-    setAreas: (areas) => set({ areas }),
+        setWards: (wards) => set({ wards }),
+        setAreas: (areas) => set({ areas }),
 
-    setWardVisibility: (wardNumber, visible) =>
-      set(
-        produce((state: LayerState) => {
-          if (!state.wardLayers[wardNumber]) {
-            state.wardLayers[wardNumber] = { visible, areas: {} };
-          } else {
-            state.wardLayers[wardNumber].visible = visible;
-          }
-        }),
-      ),
-
-    setAreaVisibility: (wardNumber, areaId, visible) =>
-      set(
-        produce((state: LayerState) => {
-          if (!state.wardLayers[wardNumber]) {
-            state.wardLayers[wardNumber] = { visible: false, areas: {} };
-          }
-          if (!state.wardLayers[wardNumber].areas) {
-            state.wardLayers[wardNumber].areas = {};
-          }
-
-          state.wardLayers[wardNumber].areas[areaId] = visible;
-        }),
-      ),
-
-    initializeWardLayer: (wardNumber, areaIds) =>
-      set(
-        produce((state: LayerState) => {
-          state.wardLayers[wardNumber] = {
-            visible: false,
-            areas: areaIds.reduce(
-              (acc, id) => {
-                acc[id] = false;
-                return acc;
+        setWardVisibility: (wardNumber, visible) =>
+          set((state) => ({
+            wardLayers: {
+              ...state.wardLayers,
+              [wardNumber]: {
+                ...state.wardLayers[wardNumber],
+                visible,
               },
-              {} as Record<string, boolean>,
-            ),
-          };
+            },
+          })),
+
+        setAreaVisibility: (wardNumber, areaId, visible) =>
+          set((state) => ({
+            wardLayers: {
+              ...state.wardLayers,
+              [wardNumber]: {
+                ...state.wardLayers[wardNumber],
+                areas: {
+                  ...state.wardLayers[wardNumber]?.areas,
+                  [areaId]: visible,
+                },
+              },
+            },
+          })),
+
+        initializeWardLayer: (wardNumber, areaIds) =>
+          set((state) => ({
+            wardLayers: {
+              ...state.wardLayers,
+              [wardNumber]: {
+                visible: false,
+                areas: Object.fromEntries(areaIds.map((id) => [id, false])),
+              },
+            },
+          })),
+
+        // Add UI state
+        isControlOpen: false,
+        setControlOpen: (open) => set({ isControlOpen: open }),
+      }),
+      {
+        name: "layer-store", // unique name for localStorage
+        partialize: (state) => ({
+          wardLayers: state.wardLayers, // only persist layer visibility state
         }),
-      ),
-  })),
+      },
+    ),
+  ),
 );
