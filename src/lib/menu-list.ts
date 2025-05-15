48c008fe4c7923b513e31327f49f6b1898d30c4c
Julius Ceaser <trilochanbhusal000@gmail.com>
2025-05-15 14:48:15 +0545

    bug: menu list has been updated

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -14,6 +14,7 @@ import {
   Paperclip,
   Home,
   PersonStanding,
+  PlusCircle,
 } from "lucide-react";
 
 export type Role = "admin" | "superadmin" | "enumerator";
@@ -109,6 +110,12 @@ const menuConfig: Menu[] = [
     icon: Paperclip, 
     roles: ["admin", "superadmin"],
     submenus: [],
+  },
+   {
+    href: "/aggregate",
+    label: "Aggregate Data",
+    icon: PlusCircle,
+    roles: ["admin", "superadmin", "enumerator"],
   },
   {
     href:"/wardwise",
